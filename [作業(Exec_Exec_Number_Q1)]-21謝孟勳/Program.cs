831d2b41a7b850202e04a7955b6fed55d81ff909
shiuns <shiunworks@gmail.com>
2022-10-21 05:03:32 +0800

    加入專案檔案。

---
+++ [作業(Exec_Exec_Number_Q1)]-21謝孟勳/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_Exec_Number_Q1___21謝孟勳
+{
+    //判斷某數值是不是質數
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入一個數值: ");
+            string input = Console.ReadLine();
+            int number =Convert.ToInt32(input);
+            int root = (int)Math.Ceiling(Math.Sqrt(number));
+            
+            for (int i = 2; i < root; i++)
+            {
+                if (number % i==0)
+                {
+                    Console.WriteLine("不是質數");
+                    return;
+                }
+                
+            }
+            Console.WriteLine("是質數");
+        }
+    }
+}
